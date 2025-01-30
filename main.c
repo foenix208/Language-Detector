e0f57b19036569fe85e157da3efddae7400fa161
ubuntu <foenix208@gmail.com>
2025-01-30 10:47:20 +0100

    main & data/text1/2

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello world");
+    FILE *fichier;
+    char buffer[500];
+    char alpha[27];
+
+
+    fichier = fopen("./data/text1.txt","r");
+    if (fichier == NULL)
+    {
+        printf("Le fichier n'a pas pue etre ouvert");
+        return 1;
+    }
+    if (fgets(buffer, 500, fichier) != NULL)    
+        printf("%s\n", buffer);
+
+    fclose(fichier);
     return 0;
 }
